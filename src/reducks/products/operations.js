ce3e940ffbef33285c549566d9bc580fdb35b726
rk1quol <kmrrt@rknoMacBook-Pro.local>
2021-05-29 22:51:38 +0900

    feat: 商品情報の更新、サイズリストの設定機能

--- src/reducks/products/operations.js
+++ src/reducks/products/operations.js
@@ -2,25 +2,30 @@ import {db, FirebaseTimestamp} from "../../firebase";
 import {push} from 'connected-react-router'
 const productsRef = db.collection('products')
 
-export const saveProduct = (category, description, gender, images, name, price) => {
+export const saveProduct = (id, category, description, gender, images, name, price, sizes) => {
   return async (dispatch) => {
     const timestamp = FirebaseTimestamp.now()
     const data = {
+      id,
       category,
       description,
       gender,
       images,
       name,
       price: parseInt(price, 10),
+      sizes,
       updated_at: timestamp
     }
 
-    const ref = productsRef.doc()
-    const id = ref.id
-    data.id = id
-    data.created_at = timestamp
+    // 新規登録の場合の処理
+    if (id === '') {
+      const ref = productsRef.doc()
+      data.created_at = timestamp
+      id = ref.id
+      data.id = id
+    }
 
-    return productsRef.doc(id).set(data)
+    return productsRef.doc(id).set(data, {merge: true})
       .then(() => {
         dispatch(push('/'))
       })
