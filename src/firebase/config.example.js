f739c2c95d3ab27117e55fe86b7d9dc8651b0302
rk1quol <kmrrt@rknoMacBook-Pro.local>
2021-07-02 00:26:03 +0900

    20210701

---
+++ src/firebase/config.example.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "willrewrite",
+  authDomain: "ec-app-12345.firebaseapp.com",
+  projectId: "ec-app-12345",
+  storageBucket: "ec-app-12345.appspot.com",
+  messagingSenderId: "687164208220",
+  appId: "1:1234567890:web:1234567890",
+  measurementId: "G-ABCDEFGHIJK"
+};
